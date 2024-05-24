f138e1f093f9a092ba0a1de837322bae825e2f24
thycrescendo <dmicholn@gmail.com>
2024-05-24 07:06:52 +0100

    updated the todo.js file

--- packages/hardhat/test/Todo.js
+++ packages/hardhat/test/Todo.js
@@ -1,28 +1,26 @@
-import { expect } from "chai";
-import { ethers } from "hardhat";
-import { YourContract } from "../typechain-types";
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-describe("YourContract", function () {
-  // We define a fixture to reuse the same setup in every test.
+describe("Todo", function () {
+  let todo;
 
-  let yourContract: YourContract;
   before(async () => {
     const [owner] = await ethers.getSigners();
-    const yourContractFactory = await ethers.getContractFactory("YourContract");
-    yourContract = (await yourContractFactory.deploy(owner.address)) as YourContract;
-    await yourContract.waitForDeployment();
+    const Todo = await ethers.getContractFactory("Todo");
+    todo = await Todo.deploy(owner.address);
+    await todo.deployed();
   });
 
   describe("Deployment", function () {
     it("Should have the right message on deploy", async function () {
-      expect(await yourContract.greeting()).to.equal("Building Unstoppable Apps!!!");
+      expect(await todo.greeting()).to.equal("Building Unstoppable Apps!!!");
     });
 
     it("Should allow setting a new message", async function () {
       const newGreeting = "Learn Scaffold-ETH 2! :)";
 
-      await yourContract.setGreeting(newGreeting);
-      expect(await yourContract.greeting()).to.equal(newGreeting);
+      await todo.setGreeting(newGreeting);
+      expect(await todo.greeting()).to.equal(newGreeting);
     });
   });
 });
